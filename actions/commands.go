bcf3476b5c3e21349d44740f842d092837ad6831
Liam Hampton <liam.hampton@ibm.com>
2019-08-14 14:49:32 +0100

    add option to remove images based on tag
    Signed-off-by: Liam Hampton <liam.hampton@ibm.com>

--- actions/commands.go
+++ actions/commands.go
@@ -129,9 +129,20 @@ func Commands() {
 		{
 			Name:    "remove",
 			Aliases: []string{"rm"},
-			Usage:   "Remove Codewind/Project docker images and the codewind network",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "tag, t",
+					Value: "latest",
+					Usage: "dockerhub image tag",
+				},
+				cli.BoolFlag{
+					Name:  "alltags, at",
+					Usage: "remove all tagged codewind images",
+				},
+			},
+			Usage: "Remove Codewind/Project docker images and the codewind network",
 			Action: func(c *cli.Context) error {
-				RemoveCommand()
+				RemoveCommand(c)
 				return nil
 			},
 		},
