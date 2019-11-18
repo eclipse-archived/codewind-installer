5c4be923f5bafbc92214b2d629c27e35058190b6
James Wallis <james.wallis1@ibm.com>
2019-11-18 13:45:30 +0000

    Add test for PFEOriginFromConnection
    Signed-off-by: James Wallis <james.wallis1@ibm.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"testing"
 
+	"github.com/eclipse/codewind-installer/pkg/connections"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,6 +25,35 @@ func TestPFEOriginError(t *testing.T) {
 			assert.Equal(t, "", got)
 			assert.Equal(t, err.Op, test.wantedErrorOp)
 			assert.Equal(t, err.Desc, test.wantedErrorDesc)
+		})
+	}
+}
+
+func TestPFEOriginFromConnection(t *testing.T) {
+	tests := map[string]struct {
+		connection    *connections.Connection
+		url           string
+		wantedErrorOp string
+	}{
+		"get templates of all styles": {
+			connection: &connections.Connection{
+				ID:       "123",
+				Label:    "connection1",
+				URL:      "ibm.com",
+				AuthURL:  "ibm.com/auth",
+				Realm:    "codewind",
+				ClientID: "codewind",
+				Username: "developer",
+			},
+			url:           "ibm.com",
+			wantedErrorOp: "config_connection_notfound",
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := PFEOriginFromConnection(test.connection)
+			assert.Equal(t, test.url, got)
+			assert.Nil(t, err)
 		})
 	}
 }
