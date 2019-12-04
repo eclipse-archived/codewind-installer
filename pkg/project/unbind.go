0811fb68b116a6c05f8d2d7736f376583699915f
Julie Stalley <julie_stalley@uk.ibm.com>
2019-12-04 14:05:42 +0000

    Add project remove command
    Signed-off-by: Julie Stalley <julie_stalley@uk.ibm.com>

---
+++ pkg/project/unbind.go
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package project
+
+import (
+	"net/http"
+
+	"github.com/eclipse/codewind-installer/pkg/config"
+	"github.com/eclipse/codewind-installer/pkg/connections"
+	"github.com/eclipse/codewind-installer/pkg/utils"
+)
+
+// Unbind a project from Codewind
+func Unbind(httpClient utils.HTTPClient, conID, projectID string) error {
+	conInfo, conInfoErr := connections.GetConnectionByID(conID)
+	if conInfoErr != nil {
+		return conInfoErr.Err
+	}
+	conURL, conErr := config.PFEOriginFromConnection(conInfo)
+	if conErr != nil {
+		return conErr.Err
+	}
+	req, err := http.NewRequest("POST", conURL+"/api/v1/projects/"+projectID+"/unbind", nil)
+	if err != nil {
+		return err
+	}
+
+	// send request
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	return nil
+}
