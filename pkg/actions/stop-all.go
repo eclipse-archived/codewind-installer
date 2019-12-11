0470f0eabc18d2884fa060cd38eb8321b636802e
James Wallis <james.wallis1@ibm.com>
2019-12-11 09:42:14 +0000

    Delete by container name not image (#283)
    Signed-off-by: James Wallis <james.wallis1@ibm.com>

--- pkg/actions/stop-all.go
+++ pkg/actions/stop-all.go
@@ -15,31 +15,19 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/docker/docker/api/types"
 	"github.com/eclipse/codewind-installer/pkg/utils"
 )
 
 //StopAllCommand to stop codewind and project containers
 func StopAllCommand() {
-	containerArr := []string{
-		"codewind-pfe",
-		"codewind-performance",
-		"cw-",
-		"appsody",
-	}
-
 	containers := utils.GetContainerList()
 
 	fmt.Println("Stopping Codewind and Project containers")
-	for _, container := range containers {
-		for _, key := range containerArr {
-			if strings.HasPrefix(container.Image, key) {
-				if key != "appsody" || strings.Contains(container.Names[0], "cw-") {
-					fmt.Println("Stopping container ", container.Names[0], "... ")
-					utils.StopContainer(container)
-					break
-				}
-			}
-		}
+	containersToRemove := getContainersToRemove(containers)
+	for _, container := range containersToRemove {
+		fmt.Println("Stopping container ", container.Names[0], "... ")
+		utils.StopContainer(container)
 	}
 
 	networkName := "codewind"
@@ -51,4 +39,23 @@ func StopAllCommand() {
 			utils.RemoveNetwork(network)
 		}
 	}
+}
+
+func getContainersToRemove(containerList []types.Container) []types.Container {
+	codewindContainerPrefixes := []string{
+		"/codewind-pfe",
+		"/codewind-performance",
+		"/cw-",
+	}
+
+	containersToRemove := []types.Container{}
+	for _, container := range containerList {
+		for _, prefix := range codewindContainerPrefixes {
+			if strings.HasPrefix(container.Names[0], prefix) {
+				containersToRemove = append(containersToRemove, container)
+				break
+			}
+		}
+	}
+	return containersToRemove
 }
