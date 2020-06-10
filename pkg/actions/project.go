ebc2fd8736f4f24ad8d40dd3861df15a0303466a
Julie Stalley <julie_stalley@uk.ibm.com>
2020-06-10 14:33:08 +0100

    Merge changes
    Signed-off-by: Julie Stalley <julie_stalley@uk.ibm.com>

--- pkg/actions/project.go
+++ pkg/actions/project.go
@@ -69,30 +69,13 @@ func ProjectCreate(c *cli.Context) {
 		HandleProjectError(projErr)
 		os.Exit(1)
 	}
-
-	if !printAsJSON {
+	if printAsJSON {
+		jsonResponse, _ := json.Marshal(result)
+		logr.Tracef(string(jsonResponse)) // won't result in multiple JSON object output unless tracing
+	} else {
 		logr.Tracef("Project downloaded to %v", destination)
 	}
-	// validate the new project
-	response, projectErr := project.ValidateProject(c)
-	if projectErr != nil {
-		fmt.Println(projectErr.Error())
-		os.Exit(1)
-	}
-
-	if printAsJSON {
-		type CreateResult struct {
-			Status           string                      `json:"status"`
-			StatusMessage    string                      `json:"status_message"`
-			ValidationResult *project.ValidationResponse `json:"validation_result"`
-		}
-		jsonResponse, _ := json.Marshal(CreateResult{Status: result.Status, StatusMessage: result.StatusMessage, ValidationResult: response})
-		logr.Tracef(string(jsonResponse))
-	} else {
-		projectInfo, _ := json.Marshal(response)
-		fmt.Println(string(projectInfo))
-	}
-	os.Exit(0)
+	ProjectValidate(c)
 }
 
 // ProjectSync : Does a project Sync
